8ed01920d16241452a60ae574df99ef784493ff6
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-04-11 23:27:13 +0530

    user schema done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import authRoles from "../utils/authroles.js";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import config from "../config/index.js";
+import crypto from "crypto"
 
 const userSchema=new mongoose.Schema({
 name:{
@@ -27,5 +31,31 @@ forgetPasswordToken:String,
 forgetPasswordExpiry:Date
 
 },{timestamps:true})
+userSchema.pre("save",async function(next){
+    if(!this.isModified('password'))return next()
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods={
+    comparePassword:async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+    getJWTtoken:function(){
+        JWT.sign({_id:this._id,role:this.role},config.JWT_SECRET,{
+            expresIn:config.JWT_EXPIRY
+        })
+    },
+    generateForgetPasswordToken:function(){
+        const forgotToken=crypto.randomBytes(20).toString('hex')
+        this.forgotPasswordToken=crypto
+        .createHash("sha256")
+        .update(forgotToken)
+        .digest("hex")
+
+        this.forgotPasswordExpiry=Date.now()+20*60*1000
+
+        return forgotToken
+    }
+}
 
 export default mongoose.model('User',userSchema)
