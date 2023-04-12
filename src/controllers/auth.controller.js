6b8f646b972ec7495c2d05d4b6a917552ba83d83
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-04-12 15:46:29 +0530

    upto auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,72 @@
+import User from '../models/user.model.js'
+import asyncHandler from '../service/asyncHandler.js'
+import CustomError from '../utils/customError.js'
+
+export const cookieOptions={
+    expires:new Date(Date.now()+3*24*60*60*1000),
+    httpOnly:true
+}
+
+export const signUp=asyncHandler(async(req,res)=>{
+    const {name,email,password}=req.body
+    if(!name||!email||!password){
+        throw new CustomError("Please add all fields",400)
+    }
+
+    const existingUser=await User.findOne({email})
+    if(existingUser){
+        throw new CustomError("user already exists",400)
+    }
+
+    const user=await User.create({
+        name,
+        email,
+        password
+    })
+
+    const token=user.getJWTtOKEN()
+
+    user.password=undefined
+    res.cookie("token",token,cookieOptions)
+
+    res.status(200).json({
+        success:true,
+        token,
+        user
+    })
+})
+
+export const login=asyncHandler(async(req,res)=>{
+    const {email,password}=req.body
+    if(!email||!password){
+        throw new CustomError("Please fill all details",400)
+    }
+    const user=User.findOne({email}).select("+password")
+
+    if(!user){
+        throw new CustomError("Invalid credentials",400)
+    }
+    const isPasswordMatched=await user.comparePassword(password)
+    if(isPasswordMatched){
+        const token=user.getJWTtoken()
+        user.password=undefined
+        res.cookie("token",token,cookieOptions)
+        return res.status(200).json({
+            success:true,
+            token,
+            user
+        })
+    }
+    throw new CustomError('password is incorrect',400)
+})
+
+export const logout=asyncHandler(async(req,res)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+    res.status(200).json({
+        success:true,
+        message:'Logged Out'
+    })
+})
---
+++ src/models/product.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:['true','please provide a product name'],
+        trim:true,
+        maxLength:[120,'product name should not be more than 120 char']
+
+    },
+    price:{
+        type:Number,
+        required:['true','please provide product price'],
+        maxLength:[5,'product name should not be more than 5 chars']
+    },
+    description:{
+        type:String
+    },
+    photos:[
+        {
+            secure_url:{
+                type:String,
+                required:true
+            }
+
+        }
+    ],
+    stock:{
+        type:Number,
+        default:0
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+    collectionId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Collection"
+    }
+    
+},{timestamps:true})
+
+
+export default mongoose.model("Product",productSchema)
