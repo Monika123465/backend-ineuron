19e47e0b9aa40c7490e9b34c75fa2a49d6aa549b
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-04-13 21:10:23 +0530

    backend ecomm

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-// const express=require('express')
+
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import express from 'express';
 
 const app=express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
 
 
 
